cf73506bd5d2578e9df15ad9f763d3070cee7b36
christofs <c.schoech@gmail.com>
2022-11-11 09:42:00 +0100

    All plots for all verbs and per category

--- analysis.py
+++ analysis.py
@@ -1,117 +1,146 @@
 """
 Script to analyze and visualize the "inner life" data from ELTeC. 
 For each language represented in the dataset, produces 
-- boxplots summarizing the data per decade, 
-- scatterplots with a regression line
-- comparison plots for the earlier vs. the later part of the data.
+- a scatterplot with a regression line, for all verbs jointly, per novel
+- a boxplot, for all verbs jointly, summarized per decade 
+- a comparison plot for the earlier vs. the later part of the data.
+- the same series of plots for each category of verbs separately.
 """
+
 
 # === Imports ===
 
+#== Basics
 import os
-from os.path import join
-import pandas as pd
-import numpy as np
 import random
-from matplotlib import pyplot as plt
-import seaborn as sns
-from scipy.stats import mannwhitneyu
 import re
+from os.path import join
+
+import numpy as np
+#== Data
+import pandas as pd
+import seaborn as sns
+# Visualization
+from matplotlib import pyplot as plt
+from scipy.stats import mannwhitneyu
 
 # === Global variables ===
 
 workdir = join(os.path.realpath(os.path.dirname(__file__)))
-categoryfile = join(workdir, "data", "innerVerbs_clean.xml")
 corpora = ["rom", "deu", "eng", "fra", "hun", "por", "spa", "slv", "por"]
+categories = ["perception", "cognition", "volition", "affect", "physiology", "moral"]
 comparison = [(1840,1869), (1889,1919)]
 samplesize = 20
 
 
 # === Functions === 
 
-def read_data(dataset): 
+def prepare_data(dataset):
+    """
+    From the original ".dat" file, calculate a few more columns: 
+    Relative frequency of all verbs; decade as a function of year. 
+    Also, relative frequency for all verbs in one category.
+    """
+    #== Read the corpus dataset from file
     with open(dataset, "r", encoding="utf8") as infile: 
         data = pd.read_csv(infile, sep=" ")
         #print(data.head())
-    return data
-
-
-def prepare_data(data):
     #== Calculate the relative frequency of the inner verbs as a proportion of all verbs.
-    # This is done at the level of all verbs of inner life as a whole 
-    # and at the level of each individual novel, which each has a year of publication.
+    #== This is done at the level of all verbs of inner life as a whole 
+    #== and at the level of each individual novel, which each has a year of publication.
     data["innerVerbsRel"] = data["innerVerbs"] / data["verbs"] 
     #== Create bins per decade
-    # This will be used for the boxplots per decade. 
+    #== This will be used for the boxplots per decade. 
     data["decade"] = (data["year"]//10)*10
-    #== Select only the relevant columns for the visualization
-    # We only need year, decade and the relative frequency of the verbs of inner life. 
-    data = data[["year", "decade", "innerVerbsRel"]]
-    #print(data.head())
+    #== Create the per-category data
+    #== This creates one column for the sum of the frequencies of each category of verbs
+    #== The value is the relative frequency calculated against all verbs
+    for category in categories: 
+        data[category] = np.sum(data.filter(regex=category), axis=1) / data["verbs"]
     return data
 
 
-def visualize_data(data, corpus): 
+def make_boxplot(corpus, data, category): 
+    """
+    This creates a boxplot of the data per decade.
+    If a specific category of verb is indicated, only this is shown. 
+    """
+    #== Select only the relevant columns for the visualization
+    if category == "all": 
+        selection = "innerVerbsRel"
+    else: 
+        selection = category
+    selected = data[["decade", selection]]
     #== Create a boxplot by decade
     plt.figure()
-    title="Verbs of inner life in ELTeC-" + corpus
-    xlabel="Relative frequency"
-    ylabel="Decades"
-    plot = sns.boxplot(data=data, x="decade", y="innerVerbsRel", palette="Blues")
-    plot.set(xlabel=xlabel, ylabel=ylabel, title=title)
+    title = "Verbs of inner life (" + category + ") in ELTeC-" + corpus
+    ylabel = "Relative frequency"
+    xlabel = "Decades"
+    plot = sns.boxplot(data = selected, x = "decade", y = selection, palette = "Blues")
+    plot.set(xlabel = xlabel, ylabel = ylabel, title = title)
     fig = plot.get_figure()
-    boxplotname = join(workdir, "results", corpus + "-boxplot-decades.png")
+    boxplotname = join(workdir, "results", corpus + "_" + category + "-byDecade.png")
     fig.savefig(join(workdir, boxplotname), dpi=300)
     plt.close()
-    #== Create a scatterplot with regression line
-    # Here, each point is the joint frequency of all verbs of inner life in one novel.
+
+
+def make_regplot(corpus, data, category): 
+    """
+    Create a scatterplot with regression line. 
+    Each point is one novel. 
+    """
+    #== Select the relevant data.
+    if category == "all": 
+        selection = "innerVerbsRel"
+    else: 
+        selection = category
+    selected = data[["decade", selection]]
     plt.figure() 
-    title="Verbs of inner life in ELTeC-" + corpus
-    xlabel="Relative frequency"
-    ylabel="Years"
-    plot = sns.regplot(data=data, x="year", y="innerVerbsRel", order=1)
-    plot.set(xlabel=xlabel, ylabel=ylabel, title=title)
+    title = "Verbs of inner life (" + category + ") in ELTeC-" + corpus
+    ylabel = "Relative frequency"
+    xlabel = "Years"
+    plot = sns.regplot(data = data, x = "year", y = selection, order = 2)
+    plot.set(xlabel = xlabel, ylabel = ylabel, title = title)
     fig = plot.get_figure()
-    regplotname = join(workdir, "results", corpus + "-scatter+regression.png")
+    regplotname = join(workdir, "results", corpus + "_" + category + "-perNovel.png")
     fig.savefig(join(workdir, regplotname), dpi=300)
     plt.close()
 
 
-def filter_data(data, comparison, samplesize): 
+def create_comparisondata(data, comparison, samplesize, category): 
     """
     Selects the average sentence length values for all texts that fall 
     into the two periods specified by the comparison parameter. 
     Returns two Series with values for each of the two periods. 
     """
+    #== Select the relevant data.
+    if category == "all": 
+        selection = "innerVerbsRel"
+    else: 
+        selection = category
     #== First series of data
     vals1 = data[(data["year"] >= comparison[0][0]) & (data["year"] <= comparison[0][1])]
-    vals1 = list(vals1.loc[:,"innerVerbsRel"])
+    vals1 = list(vals1.loc[:,selection])
     #print("vals1", len(vals1))
     vals1 = random.sample(vals1, samplesize)
     med1 = np.median(vals1)
     vals1 = pd.Series(vals1, name=str(comparison[0][0]) + "–" + str(comparison[0][1])+"\n(median="+'{0:.2f}'.format(med1)+")")
     #== Second series of data
     vals2 = data[(data["year"] >= comparison[1][0]) & (data["year"] <= comparison[1][1])]
-    vals2 = list(vals2.loc[:,"innerVerbsRel"])   
+    vals2 = list(vals2.loc[:,selection])   
     #print("vals2", len(vals2))
     vals2 = random.sample(vals2, samplesize)
     med2 = np.median(vals2)
     vals2 = pd.Series(vals2, name=str(comparison[1][0]) + "–" + str(comparison[1][1])+"\n(median="+'{0:.2f}'.format(med2)+")")
-    return vals1, vals2, med1, med2
-
-
-def test_significance(vals1, vals2): 
-    """
-    Perform a significance test for the difference between the two distributions.
-    At the moment, based only on the sample selected above, not on the full data.
-    """
     stat, p = mannwhitneyu(vals1, vals2)
-    return stat, p
+    #== Perform a significance test for the difference between the two distributions.
+    #== Based only on the sample selected above, not on the full data.
+    return vals1, vals2, med1, med2, stat, p
 
 
-def plot_seaborn(corpus, comparison, vals1, vals2, med1, med2, samplesize, p): 
+def make_kdeplot(corpus, comparison, vals1, vals2, med1, med2, samplesize, p, category): 
     """
-    Creates a plot that compares the distributions 
+    Creates a plot that compares the two selected distributions.
     """
     #== Labels
     if p < 0.00001: 
@@ -119,28 +148,15 @@ def plot_seaborn(corpus, comparison, vals1, vals2, med1, med2, samplesize, p):
     else: 
         pval = '{0:.5f}'.format(p)
     plt.figure() 
-    title="Comparison of verbs of inner life in ELTeC-" + corpus
-    xlabel="Relative frequency\n(samplesize="+str(samplesize)+", p="+pval+")"
-    ylabel="Density (KDE)"
+    title = "Comparison of verbs of inner life (" + category + ") in ELTeC-" + corpus
+    xlabel = "Relative frequency\n(samplesize=" + str(samplesize) + ", p=" + pval + ")"
+    ylabel = "Density (KDE)"
     #== Plotting
     plot = sns.displot([vals1, vals2], kind="kde", fill=True, rug=False, linewidth=2)
     plot.set(xlabel=xlabel, ylabel=ylabel, title=title)
-    complotname = join(workdir, "results", corpus + "-comparison.png")
+    complotname = join(workdir, "results", corpus + "_" + category + "-comparison.png")
     plot.savefig(join(workdir, complotname), dpi=300)
     plt.close()
-
-
-def get_categories(): 
-    with open(categoryfile, "r", encoding="utf8") as infile: 
-        data = infile.read()
-    categorydata = {}
-    lemmas = re.findall("<lemma (.*?)/>", data)
-    for item in lemmas: 
-        form = re.findall("form=\"(.*?)\"", item)[0]
-        cat = re.findall("cat=\"(.*?)\"", item)[0]
-        categorydata[form] = cat
-    #print(categorydata)
-    return categorydata
 
 
 
@@ -148,19 +164,25 @@ def get_categories():
 
 def main(): 
     for corpus in corpora: 
-        #== Prepare the data
-        dataset = join(workdir, "data", corpus, "innerVerbCounts.dat")
-        data = read_data(dataset)
-        data = prepare_data(data)
-        #== Create some plots (boxplot, regplot)
-        visualize_data(data, corpus)
-        #== Compare two time-frames
-        if corpus in ["deu", "eng", "fra", "hun", "por", "spa"]:
-            vals1, vals2, med1, med2 = filter_data(data, comparison, samplesize)
-            stat, p = test_significance(vals1, vals2)
-            plot_seaborn(corpus, comparison, vals1, vals2, med1, med2, samplesize, p) 
-        #== Create the per-category data
-        categorydata = get_categories()
+        if corpus in ["rom", "eng", "fra", "hun", "por", "spa", "slv", "por"]: # "deu" missing data!
+            #== Prepare the data
+            dataset = join(workdir, "data", corpus, "innerVerbCounts.dat")
+            data = prepare_data(dataset)
+        if corpus in ["rom", "eng", "fra", "hun", "por", "spa", "slv", "por"]: # "deu" missing data!
+            #== Create some plots (boxplot, regplot)
+            make_boxplot(corpus, data, category="all")
+            make_regplot(corpus, data, category="all")
+        if corpus in ["eng", "fra", "hun", "por", "spa"]:
+            #== Create a comparison plot
+            vals1, vals2, med1, med2, stat, p = create_comparisondata(data, comparison, samplesize, category="all")
+            make_kdeplot(corpus, comparison, vals1, vals2, med1, med2, samplesize, p, category="all") 
+        if corpus in ["eng"]: # data missing for all but eng
+            #== Create the per-category data visualizations
+            for category in categories: 
+                make_boxplot(corpus, data, category)
+                make_regplot(corpus, data, category)
+                vals1, vals2, med1, med2, stat, p = create_comparisondata(data, comparison, samplesize, category)
+                make_kdeplot(corpus, comparison, vals1, vals2, med1, med2, samplesize, p, category) 
         print("Done:", corpus)
 
 main()
